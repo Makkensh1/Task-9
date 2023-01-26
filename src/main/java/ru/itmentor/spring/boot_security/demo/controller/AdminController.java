791637fc17be5bd5ed453bf99b63cff531016181
Makkensh1 <61844918+Makkensh1@users.noreply.github.com>
2023-01-26 21:33:55 +0300

    done

--- src/main/java/ru/itmentor/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/controller/AdminController.java
@@ -2,6 +2,8 @@ package ru.itmentor.spring.boot_security.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.itmentor.spring.boot_security.demo.model.Role;
 import ru.itmentor.spring.boot_security.demo.model.User;
@@ -9,6 +11,7 @@ import ru.itmentor.spring.boot_security.demo.service.RoleService;
 import ru.itmentor.spring.boot_security.demo.service.UserService;
 
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin")
@@ -22,51 +25,55 @@ public class AdminController {
         this.roleService = roleService;
     }
 
-    @GetMapping("/users")
-    public String getUsers(Model model) {
-        model.addAttribute("users1", userService.listUsers());
-        return "users";
-    }
 
     @RequestMapping("")
-    public String adminMain() {
-        return "admin";
+    public String adminMain(Model model) {
+        model.addAttribute("users", userService.listUsers());
+        model.addAttribute("user", new User());
+        List<Role> roles = roleService.getAllRoles();
+        model.addAttribute("allRoles", roles);
+        return "users";
     }
 
     @GetMapping("/update/{id}")
     public String updateUser(@PathVariable(value = "id") Long id, Model model) {
+        List<Role> roleList = roleService.getAllRoles();
+        model.addAttribute("allRoles", roleList);
         User user = new User();
         model.addAttribute("user", user);
         user.setId(id);
-        List<Role> roleList = roleService.getAllRoles();
-        model.addAttribute("allRoles", roleList);
         return "update";
     }
 
     @PostMapping("update")
     public String update(@ModelAttribute("user") User user) {
         userService.update(user);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
     @GetMapping("delete/{id}")
     public String deleteUser(@PathVariable(value = "id") Long id) {
         userService.deleteUserById(id);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
-    @PostMapping("/save")
-    public String saveUser(@ModelAttribute("user") User user) {
+    @PostMapping("/addUser")
+    public String saveUser(@Validated(User.class) @ModelAttribute("user") User user,@RequestParam("authorities") List<String> values, BindingResult result) {
+        if(result.hasErrors()) {
+            return "error";
+        }
+        Set<Role> roleSet = roleService.getSetOfRoles(values);
+        user.setRoles(roleSet);
         userService.addUser(user);
-        return "redirect:/admin/users";
+        return "redirect:/admin";
     }
 
-    @GetMapping("/addNew")
+    @GetMapping("/addUser")
     public String addNewUser(Model model) {
-        List<Role> roleList = roleService.getAllRoles();
-        model.addAttribute("allRoles", roleList);
         User user = new User();
         model.addAttribute("user", user);
+        List<Role> roleList = roleService.getAllRoles();
+        model.addAttribute("allRoles", roleList);
         return "newUser";
     }
 }
