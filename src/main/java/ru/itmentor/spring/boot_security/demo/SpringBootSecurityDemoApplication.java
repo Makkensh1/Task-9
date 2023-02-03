099ac5ebbdfba578fe34a5682478ad5e47462dc6
Makkensh1 <61844918+Makkensh1@users.noreply.github.com>
2023-02-03 19:47:10 +0300

    done

--- src/main/java/ru/itmentor/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/SpringBootSecurityDemoApplication.java
@@ -2,14 +2,12 @@ package ru.itmentor.spring.boot_security.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringBootSecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSecurityDemoApplication.class, args);
+    }
 
 }
